9254069d88ff6487aff9d4252efe5c30befc6bce
Bryan Cairns <bcairns@voidrealms.com>
2021-06-24 08:46:19 -0400

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,7 @@ class _State extends State<MyApp> {
   int _value1 = 0;
   int _value2 = 0;
 
+  /*
   void _setValue1(int value) => setState(() => _value1 = value);
   void _setValue2(int value) => setState(() => _value2 = value);
 
@@ -29,6 +30,21 @@ class _State extends State<MyApp> {
     Column column = new Column(children: list,);
     return column;
   }
+  */
+  
+  
+void _setValue1(int? value) => setState(()=> _value1 = value!);
+void _setValue2(int? value) => setState(()=> _value2 = value!);
+ 
+Widget makeRadios () {
+ 
+  List<Widget> list = <Widget> [];
+  for (int i=0; i < 3; i ++){
+    list.add(new Radio(value: i, groupValue: _value1, onChanged: _setValue1));
+  }
+  Column column= Column (children: list ,);
+  return column;
+}
 
   Widget makeRadioTiles() {
     List<Widget> list = new List<Widget>();
